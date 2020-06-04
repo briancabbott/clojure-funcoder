1cf98c24299eee6676850041581319c65d2a9fd3
Brian <briancabbott@gmail.com>
2020-06-04 12:05:54 -0600

    soc-blends common, twitter test signup

---
+++ Projects/SocialBlends/code/common/social-blends-objects-common/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to social-blends-objects-common
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ Projects/SocialBlends/code/common/social-blends-objects-common/test/social_blends_objects_common/core_test.clj
@@ -0,0 +1,7 @@
+(ns social-blends-objects-common.core-test
+  (:require [clojure.test :refer :all]
+            [social-blends-objects-common.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
